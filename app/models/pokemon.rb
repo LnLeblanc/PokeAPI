08a344b16a558aaaa6ff26fbbba34dd0f583c95e
helene <h.leblanc.dev@gmail.com>
2021-05-15 21:02:30 +0200

    Préparation des modèles pour la mise en forme des données

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,15 @@
 class Pokemon < ApplicationRecord
-  belongs_to :type
+
+  def shape(data)
+    {
+      "id"=> data["id"],
+      "name"=> data["name"],
+      "order"=> data["order"],
+      "base_experience"=> data["base_experience"],
+      "weight"=> data["weight"],
+      "height"=> data["height"],
+      "types"=> data["types"]
+    }
+  end
+
 end
